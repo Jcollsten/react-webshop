bf36ebc41859dd17526b74514c14c7a2fa255cb0
Jonas <158564983+Jcollsten@users.noreply.github.com>
2024-05-30 16:47:28 +0200

    fixing typo (gh.pages)

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ProductPage from "./Pages/Shop/ProductPage";
 const App = () => {
   return (
     <ShopContextProvider>
-      <Router basename="'/react-webshop">
+      <Router basename="/react-webshop">
         <div className="flex flex-col min-h-screen bg-gray-200">
           <Navbar />
           <div className="flex-grow overflow-hidden">
